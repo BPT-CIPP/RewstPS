e68fb4724fb60ecdf807120850ee322e64ebd977
BPT-CIPP <projects@bastionpoint.com>
2024-09-04 17:42:42 -0400

    Update profile.template.ps1

--- profile.template.ps1
+++ profile.template.ps1
@@ -8,12 +8,11 @@
 #
 # You can define helper functions, run commands, or specify environment variables
 # NOTE: any variables defined that are not environment variables will get reset after the first execution
-
-# Authenticate with Azure PowerShell using MSI.
+ Authenticate with Azure PowerShell using MSI.
 # Remove this if you are not planning on using MSI or Azure PowerShell.
-# if ($env:MSI_SECRET) {
-#     Disable-AzContextAutosave -Scope Process | Out-Null
-#     Connect-AzAccount -Identity
-# }
+ if ($env:MSI_SECRET) {
+     Disable-AzContextAutosave -Scope Process | Out-Null
+     Connect-AzAccount -Identity
+ }
 
 $ErrorActionPreference = "Stop"
